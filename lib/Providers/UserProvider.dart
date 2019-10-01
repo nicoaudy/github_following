357bd0a57f535c76b2b6bb764562f1603e0f7f8d
Cody Mac <nicoaudy1994@gmail.com>
2019-10-01 21:18:58 +0700

    setup provider

--- lib/Providers/UserProvider.dart
+++ lib/Providers/UserProvider.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:github_following/Models/User.dart';
+import 'package:github_following/Requests/GithubRequest.dart';
 
 class UserProvider with ChangeNotifier {
   User user;
@@ -8,10 +11,47 @@ class UserProvider with ChangeNotifier {
 
   Future<bool> fetchUser(username) async {
     setLoading(true);
+    await Github(username).fetchUser().then((data) {
+      if (data.statusCode == 200) {
+        setUser(User.fromJson(json.decode(data.body)));
+      } else {
+        Map<String, dynamic> result = json.decode(data.body);
+        setMessage(result['message']);
+      }
+    });
+
+    if(isUser())
   }
 
   void setLoading(payload) {
     loading = payload;
     notifyListeners();
   }
+
+  bool isLoading() {
+    return loading;
+  }
+
+  void setUser(payload) {
+    user = payload;
+    notifyListeners();
+  }
+
+  User getUser() {
+    return user;
+  }
+
+  void setMessage(payload) {
+    errorMessage = payload;
+    notifyListeners();
+  }
+
+  String getMessage() {
+    return errorMessage;
+  }
+
+  bool isUser() {
+    return user != null ? true : false;
+  }
+
 }
