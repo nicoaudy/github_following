c71ec834b65912618b98cd08414ecd3f9958979d
Cody Mac <nicoaudy1994@gmail.com>
2019-10-01 22:06:05 +0700

    make following screen

--- lib/screens/Following.dart
+++ lib/screens/Following.dart
@@ -1,11 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:github_following/Models/User.dart';
+import 'package:github_following/Providers/UserProvider.dart';
+import 'package:provider/provider.dart';
 
-class FollowingScreen extends StatelessWidget {
+class FollowingScreen extends StatefulWidget {
+  @override
+  _FollowingScreenState createState() => _FollowingScreenState();
+}
+
+class _FollowingScreenState extends State<FollowingScreen> {
+  User user;
+  List<User> users;
+
   @override
   Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+    setState(() {
+      user = userProvider.getUser();
+    });
+
     return Scaffold(
       body: Container(
         color: Colors.white,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              brightness: Brightness.light,
+              leading: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.grey,
+              ),
+              backgroundColor: Colors.white,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      child: CircleAvatar(
+                        radius: 50.0,
+                        backgroundColor: Colors.transparent,
+                        backgroundImage: NetworkImage(user.avatar_url),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      user.login,
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
